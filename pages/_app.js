94b4cfa5cf36331bab694d2b4efc67bcf1504ce1
Laura <lauraortegama@gmail.com>
2022-04-09 19:07:24 +0200

    _document.js added; Config new theme with nextui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react'
+import customTheme from '../theme/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+            <NextUIProvider theme={customTheme}>
+                <Component {...pageProps} />
+            </NextUIProvider>
+    )
 }
 
 export default MyApp
